ab1d99a4ea599a553bb5a340b501b381b3fa1290
lwwywang <liwenwang4work@163.com>
2024-03-02 19:03:50 +0800

    修复 PC 页面动画切换时，页面内容溢出的问题

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -53,7 +53,7 @@ export const WelcomeLayout: React.FC = () => {
         <img src={logo} w-64px h-69px />
         <h1 text="#CCD2CC" text-32px>青锋账册</h1>
       </header>
-      <main shrink-1 grow-1 relative ref={main} >
+      <main shrink-1 grow-1 relative overflow-hidden ref={main} >
         {transitions((style, pathname) =>
           <animated.div key={pathname} style={{ ...style, ...extraStyle }} w="100%" h="100%" p-16px flex>
             <div grow-1 bg-white flex justify-center items-center rounded-8px>
